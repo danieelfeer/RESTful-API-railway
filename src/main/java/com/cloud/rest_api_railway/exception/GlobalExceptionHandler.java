f32c4462ac27629099adf6011cea330d040c9fb4
danieelfeer <danielfernandesbeserra26@gmail.com>
2024-10-14 15:28:08 -0300

    Add global exception handler

--- src/main/java/com/cloud/rest_api_railway/exception/GlobalExceptionHandler.java
+++ src/main/java/com/cloud/rest_api_railway/exception/GlobalExceptionHandler.java
@@ -1,4 +1,33 @@
 package com.cloud.rest_api_railway.exception;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.NoSuchElementException;
+
+@RestControllerAdvice
 public class GlobalExceptionHandler {
+
+    private final Logger logger = LoggerFactory.getLogger(GlobalExceptionHandler.class);
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<String> handleBusinessException(IllegalArgumentException busisnessException){
+        return new ResponseEntity<>(busisnessException.getMessage(), HttpStatus.UNPROCESSABLE_ENTITY);
+    }
+
+    @ExceptionHandler(NoSuchElementException.class)
+    public ResponseEntity<String> handleNotFoundException(NoSuchElementException notFoundException){
+        return new ResponseEntity<>("Resource ID not found.", HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(NoSuchElementException.class)
+    public ResponseEntity<String> handleUnexpectedException(Throwable unexpectedException){
+        var message = "Unexpected server error.";
+        logger.error(message, unexpectedException);
+        return new ResponseEntity<>(message, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
 }
