90c62a739928e5ee67cd8324a0a0d6193730f1f8
danieelfeer <danielfernandesbeserra26@gmail.com>
2024-10-14 10:38:19 -0300

    Initial commit

---
+++ src/main/java/com/cloud/rest_api_railway/RestApiRailwayApplication.java
@@ -0,0 +1,13 @@
+package com.cloud.rest_api_railway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestApiRailwayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestApiRailwayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cloud/rest_api_railway/RestApiRailwayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cloud.rest_api_railway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestApiRailwayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
